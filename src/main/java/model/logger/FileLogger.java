c8aa497e66a912ea5ceb55ea1983059a58703954
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-17 13:06:44 +0300

    added Logger interface and class FileLogger

---
+++ src/main/java/model/logger/FileLogger.java
@@ -0,0 +1,29 @@
+package model.logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FileLogger implements Logger {
+    String fileName;
+    DateTimeFormatter fileNameDate = DateTimeFormatter.ofPattern("dd-MM-yyyy" );
+
+    public FileLogger() {
+        this.fileName = fileNameDate + ".log";
+    }
+
+    public FileLogger(String fileName) {
+        this.fileName = fileName;
+    }
+    @Override
+    public void log(String str) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss " );
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
+            writer.write(LocalDateTime.now().format(formatter) + str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
